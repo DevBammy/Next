a6667b2f09a0a1b6187e04b77ed297d472429776
devBammy <devbammy@gmail.com>
2023-03-06 22:26:15 +0100

    Next Starter Files...

---
+++ pages/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./comps/Header";
+import Footer from "./comps/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
-import '@/styles/globals.css'
+import Head from "next/head";
+import "@/styles/globals.scss";
+import Layout from "../pages/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Next App | devBammy</title>
+        <meta name="description" content="My first Next.js App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="keyword"
+          content="devBammy, olorunyomi emmanuel, emmanuel olorunyomi"
+        />
+        <link rel="icon" href="/favicon.jpg" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
